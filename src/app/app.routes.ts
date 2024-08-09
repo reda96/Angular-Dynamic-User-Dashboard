4567e3fe2ffe46ec90809fb96a0c24c46d077906
reda96 <reda96>
2024-08-09 18:16:31 +0300

    add app components and structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'users', component: UsersListComponent},
+    {path: 'user/:id', component: UserDetailsComponent},
+  ];
